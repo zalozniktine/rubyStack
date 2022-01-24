9bfdcb923f165ba6cadcb587c1d82ecd1ed1ef0c
Tine <zalozniktine59@gmail.com>
2022-01-24 18:29:05 +0100

    Activity dela

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
     def activity
         @user = current_person.id
         @questions = Question.where(person_id:@user)
+
+        @comments = Answer.where.not(parent_id: nil).where(person_id:@user)
+
+        @answers = Answer.where(person_id:@user).where(parent_id: nil)
+
     end
     def questions
         @user = current_person.id
@@ -13,4 +18,6 @@ class PagesController < ApplicationController
     end
     def top
     end
+
+    
 end
---
+++ app/javascript/packs/custom.js
@@ -0,0 +1,19 @@
+function vprasanja() {
+//document.getElementById("comment").submit();
+document.getElementById('vprasanja').style.display = "none";
+document.getElementById('odgovori').style.display = "none";
+document.getElementById('komentarji').style.display = "none";
+}
+function odgovori() {
+    //document.getElementById("comment").submit();
+    document.getElementById('vprasanja').style.display = "none";
+    document.getElementById('odgovori').style.display = "none";
+    document.getElementById('komentarji').style.display = "none";
+}
+
+function komentarji() {
+    //document.getElementById("comment").submit();
+    document.getElementById('vprasanja').style.display = "none";
+    document.getElementById('odgovori').style.display = "none";
+    document.getElementById('komentarji').style.display = "none";
+}
